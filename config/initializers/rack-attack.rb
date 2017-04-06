ebb192d9f5a5e1c4874b2da3d0f6afabcc07a61d
Pank <linkinpark02111@mail.ru>
2017-04-07 00:44:15 +0300

    rack-attack update too

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -30,7 +30,7 @@ class Rack::Attack
   # Throttle all requests by IP (60rpm)
   #
   # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
-  throttle('req/ip', :limit => 5, :period => 1.second) do |req|
+  throttle('req/ip', :limit => 5, :period => 300.second) do |req|
     req.ip # unless req.path.start_with?('/assets')
   end
 
