bbe1157b36b40561f2cf923f35e76d20b969de83
Pank <linkinpark02111@mail.ru>
2017-04-07 01:01:07 +0300

    add rack-attack-allow2ban

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,6 +1,19 @@
 class Rack::Attack
 
 
+
+  # Lockout IP addresses that are hammering your login page.
+  # After 20 requests in 1 minute, block all requests from that IP for 1 hour.
+  Rack::Attack.blocklist('allow2ban login scrapers') do |req|
+    # `filter` returns false value if request is to your login page (but still
+    # increments the count) so request below the limit are not blocked until
+    # they hit the limit.  At that point, filter will return true and block.
+    Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 20, :findtime => 1.minute, :bantime => 1.hour) do
+      # The count for the IP is incremented if the return value is truthy.
+    end
+  end
+
+
   Rack::Attack.whitelist('allow from localhost') do |req|
     # Requests are allowed if the return value is truthy
     '127.0.0.1' == req.ip
