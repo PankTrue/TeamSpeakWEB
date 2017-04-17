b869158390985288e13cf41c931921978e8f3a4f
Pank <linkinpark02111@mail.ru>
2017-04-17 19:58:23 +0300

    force_encoding

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@
 require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+#Encoding.default_external = Encoding::UTF_8
+#Encoding.default_internal = Encoding::UTF_8
