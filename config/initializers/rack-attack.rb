49d515b2910da31371f9509abf7a7123232bff33
Pank <linkinpark02111@mail.ru>
2017-04-28 20:41:45 +0300

    fix

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -4,40 +4,13 @@ class Rack::Attack
   
   
   
-  # ban_ip = []
-  # unless File.file?("#{Rails.root}/log/iptables.list")
-  #   File.open "#{Rails.root}/log/iptables.list", 'a' do |f|
-  #     f.print ''
-  #   end
-  # end
-
-
-  
-  
-  
-  
   Rack::Attack.blocklist('allow2ban') do |req|
     Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 60, :findtime => 1.minute, :bantime => 10.minute) do
       req.ip == '127.0.0.1' ? false:true
     end
-    end
+  end
+
   
-  # Rack::Attack.blocklist('iptables') do |req|
-  #   Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 600, :findtime => 1.minute, :bantime => 10.minute) do
-  #       if req.ip == '127.0.0.1'
-  #         return false
-  #       else
-  #         unless ban_ip.include?(req.ip)
-  #           ban_ip << req.ip
-  #           File.open("#{Rails.root}/log/iptables.list", 'a') do |f|
-  #             f.puts req.ip
-  #           end
-  #         end
-  #       end
-  #   end
-  # end
-
-
   Rack::Attack.blocklisted_response = lambda do |env|
     [ 403, {}, ['You are banned for a large number of requests']]
   end
