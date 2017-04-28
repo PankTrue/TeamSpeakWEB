06240c8eae11bc603a8baed26fdd3ae28ec70c06
Pank <linkinpark02111@mail.ru>
2017-04-28 20:25:36 +0300

    fix

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,25 +1,41 @@
 class Rack::Attack
-  
+
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new # defaults to Rails.cache
+  
+  
+  
+  # ban_ip = []
+  # unless File.file?("#{Rails.root}/log/iptables.list")
+  #   File.open "#{Rails.root}/log/iptables.list", 'a' do |f|
+  #     f.print ''
+  #   end
+  # end
+
+
+  
+  
   
   
   Rack::Attack.blocklist('allow2ban') do |req|
     Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 60, :findtime => 1.minute, :bantime => 10.minute) do
       req.ip == '127.0.0.1' ? false:true
     end
-
     end
   
-
-
-
   # Rack::Attack.blocklist('iptables') do |req|
   #   Rack::Attack::Allow2Ban.filter(req.ip, :maxretry => 600, :findtime => 1.minute, :bantime => 10.minute) do
-  #       Rails.logger("Ban: #{req.ip}")
-  #       return true
+  #       if req.ip == '127.0.0.1'
+  #         return false
+  #       else
+  #         unless ban_ip.include?(req.ip)
+  #           ban_ip << req.ip
+  #           File.open("#{Rails.root}/log/iptables.list", 'a') do |f|
+  #             f.puts req.ip
+  #           end
+  #         end
+  #       end
   #   end
   # end
-
 
 
   Rack::Attack.blocklisted_response = lambda do |env|
@@ -34,8 +50,7 @@ class Rack::Attack
 
 
   throttle('panel', :limit => 5, :period => 10.second) do |req|
-    req.ip if req.path == "/cabinet/panel/#{req.path.split('/').last}" or
-              req.path == '/cabinet'
+    req.ip if req.path == "/cabinet/panel/#{req.path.split('/').last}" or req.path == '/cabinet'
   end
 
 
