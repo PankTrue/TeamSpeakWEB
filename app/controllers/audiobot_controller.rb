09ca0aa110593bf2484eb1e47f6c5ed507ec716a
Pank <linkinpark02111@mail.ru>
2018-03-27 23:09:01 +0300

    refactoring audiobot controller

--- app/controllers/audiobot_controller.rb
+++ app/controllers/audiobot_controller.rb
@@ -7,6 +7,10 @@ class AudiobotController < ApplicationController
   before_action :audiobot_params_for_settings_up, only: [:settings_up]
   before_action :own_bot,only: [:edit,:update,:destroy,:panel, :settings,:settings_up,:extend, :extend_up, :restart]
 
+  rescue_from Errno::ECONNREFUSED, :with => :invalid_manager
+  rescue_from ActiveRecord::RecordInvalid, :with => :invalid_transaction
+
+
   def new
     @audiobot = Audiobot.new()
   end
@@ -14,30 +18,33 @@ class AudiobotController < ApplicationController
   def create
     @audiobot = Audiobot.new(audiobot_params_for_create)
 
-    if [1,2,3,6,12].include?(params[:audiobot][:time_payment].to_i)
-      @audiobot.user_id = current_user.id
-      cost = params[:audiobot][:time_payment].to_i * Settings.audiobot.audio_quota_cost.to_f * params[:audiobot][:audio_quota].to_i
-      @audiobot.time_payment = Date.today + 30 * params[:audiobot][:time_payment].to_i
-      # @audiobot.server_id = params[:audiobot][:server_id].to_i
-      if current_user.have_money?(cost)
-        if @audiobot.valid?
-          if @audiobot.save and current_user.update(money: ((current_user.money - cost).round(2)), spent: current_user.spent+cost)
-            update_audiobot_cfg
-            audiobot_start
-            referall_system cost, current_user.ref
-            redirect_to cabinet_home_path, success:'Вы успешно купили аудио бота'
-          else
-            redirect_to audiobot_new_path(@audiobot), danger: 'Что-то пошло не так, отпишите администрации чтобы они это починили.'
-          end
-        else
-            render audiobot_new_path @audiobot
-        end
-      else
-        redirect_to cabinet_home_path, danger:'Недостаточно средств'
-      end
-    else
-      render 'new'
+     unless [1,2,3,6,12].include?(params[:audiobot][:time_payment].to_i)
+       redirect_to audiobot_new_path(@audiobot), warning: 'Не корректно выбрано время оплаты'
+       return
+     end
+
+    @audiobot.user_id = current_user.id
+    cost = params[:audiobot][:time_payment].to_i * Settings.audiobot.audio_quota_cost.to_f * params[:audiobot][:audio_quota].to_i
+    @audiobot.time_payment = Date.today + 30 * params[:audiobot][:time_payment].to_i
+
+    unless current_user.have_money?(cost)
+      redirect_to cabinet_home_path, danger:'Недостаточно средств'
+      return
     end
+    unless @audiobot.valid?
+      redirect_to audiobot_new_path(@audiobot),'Некоторые поля были введены неверно'
+      return
+    end
+
+    ActiveRecord::Base.transaction do
+      @audiobot.save!
+      current_user.update!(money: ((current_user.money - cost).round(2)), spent: current_user.spent+cost)
+      referall_system cost, current_user.ref
+      update_audiobot_cfg
+      audiobot_start
+    end
+
+    redirect_to cabinet_home_path, success: 'Вы успешно купили аудиобота'
   end
 
   def edit
@@ -47,22 +54,24 @@ class AudiobotController < ApplicationController
   def update
     days = Teamspeak::Other.sec2days(@audiobot.time_payment.to_time - Time.now)
     cost = (((params[:audiobot][:audio_quota].to_i - @audiobot.audio_quota) * (Settings.audiobot.audio_quota_cost.to_f/30*days))).round(2)
-    if current_user.have_money?(cost)
-      if @audiobot.valid?
-        ActiveRecord::Base.transaction do
-          current_user.update!(money: ((current_user.money - cost).round 2), spent: current_user.spent+=cost)
-          @audiobot.update!(audio_quota: params[:audiobot][:audio_quota].to_i)
-          referall_system cost, current_user.ref
-        end
-        update_audiobot_cfg
-        redirect_to audiobot_panel_path(params[:id]), success: 'Вы успешно редактировали бота'
-      else
-        render 'cabinet/edit'
-      end
 
-    else
+    unless current_user.have_money?(cost)
       redirect_to cabinet_home_path, danger: 'Недостаточно средст'
+      return
     end
+    unless @audiobot.valid?
+      redirect_to audiobot_edit_path(@audiobot),warning: 'Не корректно выбрано время оплаты'
+      return
+    end
+
+    ActiveRecord::Base.transaction do
+      current_user.update!(money: ((current_user.money - cost).round 2), spent: current_user.spent+=cost)
+      @audiobot.update!(audio_quota: params[:audiobot][:audio_quota].to_i)
+      referall_system cost, current_user.ref
+      update_audiobot_cfg
+    end
+
+    redirect_to audiobot_panel_path(params[:id]), success: 'Вы успешно редактировали бота'
   end
 
   def destroy
@@ -75,12 +84,11 @@ class AudiobotController < ApplicationController
   end
 
   def settings_up
-    if(@audiobot.update(audiobot_params_for_settings_up))
+    ActiveRecord::Base.transaction do
+      @audiobot.update!(audiobot_params_for_settings_up)
       update_audiobot_cfg
-      redirect_to audiobot_panel_path(@audiobot), success: 'Вы успешно изменили настройки бота'
-    else
-      render 'audiobot/settings'
     end
+    redirect_to audiobot_panel_path(@audiobot), success: 'Вы успешно изменили настройки бота'
   end
 
   def extend
@@ -88,31 +96,36 @@ class AudiobotController < ApplicationController
 
   def extend_up
     time_payment = params[:audiobot][:time_payment].to_i
-    if [1,2,3,6,12].include?(time_payment)
-      cost = @audiobot.audio_quota * Settings.audiobot.audio_quota_cost * time_payment
-      if current_user.have_money?(cost)
-        current_user.spent+= cost
-        current_user.money = current_user.money - cost
-        @audiobot.state = true
-        if Date.today <= @audiobot.time_payment
-          @audiobot.time_payment = @audiobot.time_payment + time_payment * 30
-        else
-          @audiobot.time_payment = Date.today + time_payment * 30
-        end
-          ActiveRecord::Base.transaction do
-            @audiobot.save
-            current_user.save
-            referall_system cost, current_user.ref
-          end
-          update_audiobot_cfg
-
-          redirect_to cabinet_home_path, success:'Вы успешно продлил'
-      else
-        redirect_to cabinet_home_path, danger: 'Недостаточно средств'
-      end
-    else
-      redirect_to cabinet_home_path
+    unless [1,2,3,6,12].include?(time_payment)
+      redirect_to audiobot_extend_path(@audiobot), warning: 'Не корректно выбрано время оплаты'
+      return
     end
+
+    cost = @audiobot.audio_quota * Settings.audiobot.audio_quota_cost * time_payment
+
+    unless current_user.have_money?(cost)
+      redirect_to cabinet_home_path, danger: 'Недостаточно средств'
+      return
+    end
+
+    current_user.spent+= cost
+    current_user.money = current_user.money - cost
+    @audiobot.state = true
+
+    if Date.today <= @audiobot.time_payment
+      @audiobot.time_payment = @audiobot.time_payment + time_payment * 30
+    else
+      @audiobot.time_payment = Date.today + time_payment * 30
+    end
+
+    ActiveRecord::Base.transaction do
+      @audiobot.save!
+      current_user.save!
+      referall_system cost, current_user.ref
+      update_audiobot_cfg
+    end
+
+    redirect_to cabinet_home_path, success:'Вы успешно продлил'
   end
 
   def restart
@@ -154,6 +167,14 @@ private
     TCPSocket.open(Settings.other.ip[@audiobot.server_id],Settings.audiobot.manager_port) do |sock|
       sock.write("#{Settings.audiobot.verify_data}start #{@audiobot.id}")
     end
+  end
+
+  def invalid_manager
+    redirect_to home_index_path, danger:'Проблемы с сервером, сообщите об этом администрации'
+  end
+
+  def invalid_transaction
+    redirect_to home_index_path, danger:'Что-то пошло не так, сообщите об этом администрации'
   end
 
 end
